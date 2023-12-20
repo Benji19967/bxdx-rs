b34207736702489536a50da01851cc1a1805e0cd
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-12-20 14:24:42 -0500

    Make shared state work

--- server/src/bin/axum_kv_store.rs
+++ server/src/bin/axum_kv_store.rs
@@ -21,21 +21,21 @@ struct KvEntry {
 
 type SharedState = Arc<RwLock<AppState>>;
 
-#[derive(Clone)]
+#[derive(Default)]
 struct AppState {
     db: HashMap<String, String>,
 }
 
 #[tokio::main]
 async fn main() {
-    let mut state = AppState { db: HashMap::new() };
+    let shared_state = SharedState::default();
 
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/health_check", get(health_check))
         .route("/kv/:key", get(get_key))
         .route("/kv", post(insert_key))
-        .with_state(state);
+        .with_state(Arc::clone(&shared_state));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -49,16 +49,17 @@ async fn health_check() -> &'static str {
 async fn get_key(
     // access the state via the `State` extractor
     // extracting a state of the wrong type results in a compile error
-    State(state): State<AppState>,
+    State(state): State<SharedState>,
     Path(key): Path<String>,
 ) -> Result<String, StatusCode> {
-    if let Some(value) = state.db.get(&key) {
+    if let Some(value) = state.read().unwrap().db.get(&key) {
         Ok(value.clone())
     } else {
         Err(StatusCode::NOT_FOUND)
     }
 }
 
-async fn insert_key(State(state): State<AppState>, Json(payload): Json<KvEntry>) {
-    state.db.insert(payload.key, payload.value);
+async fn insert_key(State(state): State<SharedState>, Json(payload): Json<KvEntry>) {
+    println!("Inserted {}:{}", &payload.key, &payload.value);
+    state.write().unwrap().db.insert(payload.key, payload.value);
 }
